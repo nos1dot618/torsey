5a0c6eb706b839d2df5d6e9c8be0b4740e539ad3
ninthcircle <mainlakshayhoon@gmail.com>
2026-01-17 18:54:47 +0530

    Supported contacting torrent-tracker

---
+++ src/torsey/client.py
@@ -0,0 +1,37 @@
+import hashlib
+import os
+import urllib.parse
+from urllib.request import urlopen
+
+from torsey.bencoding import encode, decode
+from torsey.metadata_info import MetadataInfo
+
+DEFAULT_PORT = 6881
+
+
+def generatePeerID() -> bytes:
+    return b"-TS0001-" + os.urandom(12)
+
+
+def constructTrackerURL(metadataInfo: MetadataInfo, port: int = DEFAULT_PORT):
+    announce = metadataInfo.getAnnounce()
+    assert announce is not None
+    params = {
+        "info_hash": hashlib.sha1(encode(metadataInfo.info)).digest(),
+        "peer_id": generatePeerID(),
+        "port": port,
+        "uploaded": 0,
+        "downloaded": 0,
+        "left": metadataInfo.totalLength(),
+        "compact": 1,
+        "event": "started"
+    }
+    query = urllib.parse.urlencode(params, quote_via=urllib.parse.quote)
+    return f"{announce}?{query}"
+
+
+def contactTracker(metadataInfo: MetadataInfo, port: int = DEFAULT_PORT):
+    trackerURL = constructTrackerURL(metadataInfo, port)
+    with urlopen(trackerURL) as response:
+        data = response.read()
+    return decode(data)
