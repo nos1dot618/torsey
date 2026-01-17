1056fe182e93580edbfada06eb45c3197e30b4f2
ninthcircle <mainlakshayhoon@gmail.com>
2026-01-17 18:53:15 +0530

    Added new test torrent-file

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ pip install -e .
 ## References
 
 1. BitTorrent Protocol Specification: <https://www.bittorrent.org/beps/bep_0003.html>
-2. Sample torrent-file: <https://sample-file.bazadanni.com/2012/01/torrent.html> 
+2. Sample torrent-file: <https://sample-file.bazadanni.com/2012/01/torrent.html>
+3. Test torrent-file: <https://academictorrents.com/details/d984f67af9917b214cd8b6048ab5624c7df6a07a>
--- tests/unit/test_torrent_loader.py
+++ tests/unit/test_torrent_loader.py
@@ -1,8 +1,13 @@
 import unittest
-from torsey.torrent_loader import decodeTorrentURL
+from torsey.torrent_loader import decodeTorrentFile, decodeTorrentURL
 
 
 class TestTorrentLoader(unittest.TestCase):
+    def testDecodeTorrentFile(self):
+        decodedTorrent = decodeTorrentFile("resources/sample.torrent")
+        self.assertIn(b"announce", decodedTorrent)
+        self.assertIn(b"info", decodedTorrent)
+
     def testDecodeTorrentURL(self):
         decodedTorrent = decodeTorrentURL(
             "https://sample-file.bazadanni.com/download/applications/torrent/sample.torrent")
